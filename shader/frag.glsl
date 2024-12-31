67fa1dfed1756479f69c9254ceb5b80011818827
Michael Mark <michael.mark@oit.edu>
2024-12-31 03:43:16 -0800

    Added point lights

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -48,6 +48,43 @@ vec3 calc_directional_light(DirectionalLight light) {
   return ambient + diffuse + specular;
 }
 
+struct PointLight {
+  vec3 position;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+  float constant;
+  float linear;
+  float quadratic;
+};
+
+vec3 calc_point_light(PointLight light) {
+  // Calculate ambient component
+  vec3 ambient = material.ambient * light.ambient *
+                 vec3(texture(material.ambient_map, vert_tex));
+
+  // Calculate diffuse component
+  vec3 light_dir = normalize(light.position - vert_pos);
+  vec3 diffuse = material.diffuse * light.diffuse *
+                 vec3(texture(material.diffuse_map, vert_tex)) *
+                 max(dot(vert_normal, light_dir), 0.0);
+
+  // Calculate specular component
+  vec3 camera_dir = normalize(camera_pos - vert_pos);
+  vec3 reflect_dir = reflect(-light_dir, vert_normal);
+  vec3 specular =
+      material.specular * light.specular *
+      vec3(texture(material.specular_map, vert_tex)) *
+      pow(max(dot(camera_dir, reflect_dir), 0.0), material.shininess);
+
+  // Calculate attenuation
+  float distance = length(light.position - vert_pos);
+  float attenuation = 1.0 / (light.constant + light.linear * distance +
+                             light.quadratic * distance * distance);
+
+  return attenuation * (ambient + diffuse + specular);
+}
+
 void main() {
   DirectionalLight light1 =
       DirectionalLight(vec3(-1.0, -1.0, -1.0), vec3(1.0, 1.0, 1.0),
@@ -57,9 +94,15 @@ void main() {
       DirectionalLight(vec3(-1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0),
                        vec3(0.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0));
 
+  PointLight light3 =
+      PointLight(vec3(0.0, 2.0, 0.0), vec3(1.0), vec3(0.0, 0.0, 1.0), vec3(1.0),
+                 1.0, 0.22, 0.2);
+
   vec3 directional_lights =
       calc_directional_light(light1) + calc_directional_light(light2);
 
+  vec3 point_lights = calc_point_light(light3);
+
   // Sum the components together
-  final_color = vec4(directional_lights, 1.0);
+  final_color = vec4(directional_lights + point_lights, 1.0);
 }
