4c825821cdd4eac956e466f6cf2e53aa0be80c64
Michael Mark <michael.mark@oit.edu>
2024-12-30 16:59:14 -0800

    Added Light and Material structs to frag

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -10,30 +10,42 @@ uniform vec3 camera_pos;
 uniform sampler2D diffuse_map;
 uniform sampler2D specular_map;
 
+struct Material {
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+  float shininess;
+};
+
+struct Light {
+  vec3 position;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
 // Phong lighting model
 void main() {
-  vec3 light_color = vec3(1.0, 1.0, 1.0);
-  vec3 light_pos = vec3(2.0, 1.2, 1.5);
+  Material material = Material(vec3(0.1), vec3(1.0), vec3(0.5), 32.0);
+  Light light = Light(vec3(2.0, 1.2, 1.5), vec3(1.0, 1.0, 0.0),
+                      vec3(0.0, 1.0, 1.0), vec3(1.0, 0.0, 1.0));
 
   // Calculate ambient component
-  float ambient_strength = 0.1;
-  vec3 ambient = ambient_strength * light_color;
+  vec3 ambient = material.ambient * light.ambient;
 
   // Calculate diffuse component
-  float diffuse_strength = 1.0;
-  vec3 light_dir = normalize(light_pos - vert_pos);
-  vec3 diffuse = diffuse_strength * light_color *
+  vec3 light_dir = normalize(light.position - vert_pos);
+  vec3 diffuse = material.diffuse * light.diffuse *
                  vec3(texture(diffuse_map, vert_tex)) *
                  max(dot(vert_normal, light_dir), 0.0);
 
   // Calculate specular component
-  float specular_strength = 0.5;
-  float shininess = 320.0;
   vec3 camera_dir = normalize(camera_pos - vert_pos);
   vec3 reflect_dir = reflect(-light_dir, vert_normal);
-  vec3 specular = specular_strength * light_color *
-                  vec3(texture(specular_map, vert_tex)) *
-                  pow(max(dot(camera_dir, reflect_dir), 0.0), shininess);
+  vec3 specular =
+      material.specular * light.specular *
+      vec3(texture(specular_map, vert_tex)) *
+      pow(max(dot(camera_dir, reflect_dir), 0.0), material.shininess);
 
   // Sum the components together
   final_color = vec4(ambient + diffuse + specular, 1.0);
